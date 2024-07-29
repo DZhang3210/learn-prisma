e658e9135509b52b923b9080005f38fe97050083
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2024-07-29 12:31:44 -0400

    new commit

--- script.ts
+++ script.ts
@@ -3,9 +3,64 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 async function main() {
-    const user = await prisma.user.findMany()
-    console.log(user)
+    // await prisma.user.deleteMany()
+    // const user = await prisma.user.createMany({
+    //     data: [
+    //         {
+    //             name: "Kyle",
+    //             email: "kyle@test.com",
+    //             age: 27,
+    //         }, {
+    //             name: "Sally",
+    //             email: "sally@test.com",
+    //             age: 32,
+    //         },
+    //     ],
+    // })
+    // console.log(user)
+    // await prisma.user.createMany({
+    //     data: [{
+    //         name: "Sally",
+    //         age: 12,
+    //         email: "sally@test1.com"
+    //     }]
+    // })
+    const user = await prisma.user.findMany({
+        where: {
+            // name: { in: ["Sally", "Kyle"] }
+            // age: { lt: 20 }
 
+            writtenPosts: {
+                every: {
+                    title: "Test"
+                }
+            }
+
+            // userPreference: {
+            //     emailUpdates: false
+            // }
+
+            // NOT: {
+            //     email: { startsWith: "sally" }  
+            // }
+            // OR:
+            //     [
+            //         {
+            //             email: { startsWith: "sally" },
+            //         },
+            //         {
+            //             email: { endsWith: "@test1.com" }
+            //         }
+            //     ]
+        },
+        // orderBy: {
+        //     age: "desc"
+        // }
+        // take: 1,
+        // skip: 1 
+        // distinct: ["name"]
+    })
+    console.log(user)
 }
 
 main()
